4ccfb9f0f27afb509ccdd35623097f950714fc07
Ly Say Hoi <l.sayhoi.001@gmail.com>
2024-01-25 23:13:23 +0700

    add cors config

--- src/main/java/com/hoi/taxmanagement/controller/TaxQueryController.java
+++ src/main/java/com/hoi/taxmanagement/controller/TaxQueryController.java
@@ -6,10 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 
--- src/main/java/com/hoi/taxmanagement/util/RestTemplateUtil.java
+++ src/main/java/com/hoi/taxmanagement/util/RestTemplateUtil.java
@@ -1,5 +1,6 @@
 package com.hoi.taxmanagement.util;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.*;
 import org.springframework.stereotype.Component;
@@ -9,6 +10,7 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 @Component
+@Slf4j
 public class RestTemplateUtil {
 
     private final RestTemplate restTemplate;
@@ -29,13 +31,13 @@ public class RestTemplateUtil {
 
     public <T> ResponseEntity<T> get(String url, Map<String, String> param, HttpHeaders headers, Class<T> responseType) {
         String uri = url + buildQueryString(param);
-        System.out.println(uri);
+        log.info("Request url: {}", uri);
         return restTemplate.exchange(uri, HttpMethod.GET, new HttpEntity<>(headers), responseType);
     }
 
     public ResponseEntity<ByteArrayResource> getFile(String url, Map<String, String> param, HttpHeaders headers) {
         String uri = url + buildQueryString(param);
-        System.out.println(uri);
+        log.info("Request url: {}", uri);
         return restTemplate.execute(uri, HttpMethod.GET, request -> request.getHeaders().addAll(headers), response -> {
             ByteArrayResource resource = new ByteArrayResource(response.getBody().readAllBytes());
             HttpHeaders responseHeaders = response.getHeaders();
